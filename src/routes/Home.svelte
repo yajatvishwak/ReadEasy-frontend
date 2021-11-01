e4e7d74a22d1960b88d7dd6e8b42ffce65f9197e
steinskeeper <dpelpsykongroo@gmail.com>
2021-11-01 17:06:42 +0530

    merge conflict

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -112,7 +112,6 @@
   class="grid grid-cols-3 {isVisualOpen ? 'blur-sm' : ''}
  bg-gray-800 h-screen text-white  "
 >
-<<<<<<< HEAD
   <div
     id="scrollcontainer"
     class="col-start-1 flex my-auto p-16 bg-white text-black w-full col-span-2  flex-wrap "
@@ -193,80 +192,6 @@
         <i class="fas fa-volume-up mx-5" />
         What is this word?
       </div>
-=======
-    <div
-        id="scrollcontainer"
-        class="col-start-1 flex my-auto p-16 bg-white text-black w-full col-span-2  flex-wrap "
-    >
-        {#each text as word, index}
-            {#if index === selectedText}
-                <span
-                    on:click={() => wordChosen(index)}
-                    id="highlighted"
-                    class="spanbro bg-black text-white p-2 cursor-pointer"
-                    style="margin:{marginValue}px;font-size: {fontValue}px;"
-                    >{word}</span
-                >
-            {:else if highlighted === index}
-                <span
-                    on:click={() => wordChosen(index)}
-                    id="highlighted"
-                    class="spanbro bg-yellow-300 text-black p-2 cursor-pointer"
-                    style=" margin:{marginValue}px;font-size: {fontValue}px;"
-                    >{word}</span
-                >
-            {:else}
-                <span
-                    on:click={() => wordChosen(index)}
-                    class="spanbro cursor-pointer"
-                    style="margin:{marginValue}px;font-size: {fontValue}px;"
-                    >{word}</span
-                >
-            {/if}
-        {/each}
-    </div>
-    <div class="col-start-3 bg-gray-700 pt-16 px-6">
-        <div class="fixed">
-            <div class="flex items-center justify-between">
-                <span class="text-3xl font-bold ">HELLO Options</span>
-            </div>
-            <div class="w-full mt-7">
-                Font Size
-                <input
-                    type="range"
-                    min="50"
-                    max="100"
-                    bind:value={fontValue}
-                    class="w-full"
-                    id="myRange"
-                />
-            </div>
-            <div class="w-full mt-4">
-                Font Spacing
-                <input
-                    type="range"
-                    min="20"
-                    max="50"
-                    bind:value={marginValue}
-                    class="w-full"
-                    id="myRange"
-                />
-            </div>
-            <div
-                class="w-full p-4 m-5 cursor-pointer hover:text-black hover:bg-white transition-all mx-0 rounded-lg border"
-                on:click={onvisualitclicked}
-            >
-                <i class="fas fa-glasses px-5 " />
-                Visualize it
-            </div>
-            <div
-                on:click={() => sayWord()}
-                class="w-full p-4 m-5 hover:text-black hover:bg-white transition-all mx-0 rounded-lg border"
-            >
-                <i class="fas fa-volume-up mx-5" />
-                What is this word?
-            </div>
->>>>>>> e160d951036839a58fc6fef4a11b7003137daa2f
 
       <div
         on:click={() => (readEachWordOut = !readEachWordOut)}
